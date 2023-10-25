4770776b1e6bd0da787d864a5f6b89cb8f4bca12
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-10-26 02:40:23 +0300

    save file

--- app/Sss/page.jsx
+++ app/Sss/page.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Faq from '../components/Faq/Faq'
-import BlogAnon from '../pages/WhyUs/BlogAnon'
-import ContactUs from '../components/Contact'
+import React from "react";
+import Faq from "../components/Faq/Faq";
+import BlogAnon from "../pages/WhyUs/BlogAnon";
+import ContactUs from "../components/Contact";
 
 const page = () => {
   return (
-      <div>
-          <Faq />
-          <BlogAnon />
-          <ContactUs />
-          
+    <div>
+      <Faq />
+      <BlogAnon />
+      <ContactUs />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
